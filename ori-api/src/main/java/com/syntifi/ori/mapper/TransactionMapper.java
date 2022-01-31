2b8e0b0e4b7ed5b0cd732f39fe2e463117924f58
AB <andre@syntifi.com>
2022-01-31 23:32:22 +0100

    adding getBlock method to TransactoinMapper

--- ori-api/src/main/java/com/syntifi/ori/mapper/TransactionMapper.java
+++ ori-api/src/main/java/com/syntifi/ori/mapper/TransactionMapper.java
@@ -3,6 +3,7 @@ package com.syntifi.ori.mapper;
 import com.syntifi.ori.dto.TransactionDTO;
 import com.syntifi.ori.exception.ORIException;
 import com.syntifi.ori.model.Account;
+import com.syntifi.ori.model.Block;
 import com.syntifi.ori.model.Transaction;
 import com.syntifi.ori.repository.AccountRepository;
 import com.syntifi.ori.repository.BlockRepository;
@@ -32,6 +33,7 @@ public class TransactionMapper {
             BlockRepository blockRepository) {
         Account fromAccount = getAccount(dto.getTokenSymbol(), dto.getFromHash(), accountRepository);
         Account toAccount = getAccount(dto.getTokenSymbol(), dto.getToHash(), accountRepository);
+        Block block = getBlock(dto.getTokenSymbol(), dto.getBlockHash(), blockRepository);
 
         return Transaction.builder()
                 .hash(dto.getHash())
@@ -39,7 +41,7 @@ public class TransactionMapper {
                 .amount(dto.getAmount())
                 .fromAccount(fromAccount)
                 .toAccount(toAccount)
-                .block(blockRepository.findByHash(dto.getTokenSymbol(), dto.getBlockHash()))
+                .block(block)
                 .build();
     }
 
@@ -50,6 +52,15 @@ public class TransactionMapper {
             throw new ORIException("Account not found", 404);
         }
         return account;
+    }
+
+    private static Block getBlock(String tokenSymbol, String hash, BlockRepository blockRepository) {
+        Block block = blockRepository.findByHash(tokenSymbol, hash);
+        // TODO: Throw with different exception and throw this @ controller level
+        if (block == null) {
+            throw new ORIException("Account not found", 404);
+        }
+        return block;
     }
 
 }
