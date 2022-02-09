255336d0b61a446cb10cb5ff7d45b1e1a9782f57
Alexandre C <adcarvalho@gmail.com>
2022-02-09 14:11:14 -0300

    updated web3j and fixed okhttp3 version included db on ethchain set spring boot maven plugin version on parent

--- ori-chains/ori-eth/src/main/java/com/syntifi/ori/chains/eth/EthChainCrawlerApplication.java
+++ ori-chains/ori-eth/src/main/java/com/syntifi/ori/chains/eth/EthChainCrawlerApplication.java
@@ -9,13 +9,12 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.web3j.protocol.Web3j;
 
 import lombok.Getter;
 
 @Getter
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @EnableBatchProcessing
 public class EthChainCrawlerApplication extends AbstractChainCrawlerJob<Web3j, EthChainData> {
     @Autowired
