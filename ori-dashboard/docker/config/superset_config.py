e38fb9522650882567ce016863b2a5e18f7e8a71
AB <andre@syntifi.com>
2022-02-23 22:30:59 +0100

    changing dashboard title

--- ori-dashboard/docker/config/superset_config.py
+++ ori-dashboard/docker/config/superset_config.py
@@ -231,7 +231,7 @@ PROXY_FIX_CONFIG = {"x_for": 1, "x_proto": 1, "x_host": 1, "x_port": 1, "x_prefi
 # GLOBALS FOR APP Builder
 # ------------------------------
 # Uncomment to setup Your App name
-APP_NAME = "Superset"
+APP_NAME = "SyntiFi Dashboard"
 
 # Specify the App icon
 APP_ICON = "/static/assets/images/logo.png"
